1324f803d8f3050e1663888aa14877fd825b21a8
Brent Yorgey <byorgey@cis.upenn.edu>
2012-02-07 03:25:46 +0000

    initial import

---
+++ src/Diagrams/Builder.hs
@@ -0,0 +1,84 @@
+{-# LANGUAGE StandaloneDeriving
+           , DeriveDataTypeable
+           , ScopedTypeVariables
+           , FlexibleContexts
+  #-}
+
+module Diagrams.Builder where
+
+import Diagrams.Prelude
+
+import Language.Haskell.Interpreter
+
+import System.IO
+import System.FilePath
+import System.Directory
+
+import Data.Typeable
+
+deriving instance Typeable Any
+
+-- | Set up the module to be interpreted, in the context of the
+--   necessary imports.
+setDiagramImports :: MonadInterpreter m 
+                  => String      -- ^ Filename of the module containing the diagrams
+                  -> [String]    -- ^ Additional necessary imports
+                  -> m ()
+setDiagramImports m imps = do
+    loadModules [m]
+    setTopLevelModules [takeBaseName m]
+    setImports $ [ "Prelude"
+                 , "Diagrams.Prelude"
+                 , "Graphics.Rendering.Diagrams.Core"
+                 , "Data.Monoid"
+                 ]
+                 ++ imps
+
+-- | Compile a 
+compileDiagram :: forall b v. 
+                  ( Typeable b, Typeable v
+                  , InnerSpace v, OrderedField (Scalar v), Backend b v
+                  )
+               => b             -- ^ Backend token
+               -> v             -- ^ Dummy vector to identify the vector space
+               -> Options b v   -- ^ Rendering options
+               -> String        -- ^ Filename of the module containing the example
+               -> [String]      -- ^ Additional imports needed
+               -> String        -- ^ Expression of type @Diagram b v@ to be compiled
+               -> IO (Either InterpreterError (Result b v))
+compileDiagram b v opts m imps exp =
+    runInterpreter $ do
+      setDiagramImports m imps
+      d <- interpret exp (as :: Diagram b v)
+      return (renderDia b opts d)
+
+{-
+ppError :: InterpreterError -> IO ()
+ppError (UnknownError e) = putStrLn $ "UnknownError: " ++ e
+ppError (WontCompile es) = putStr . unlines . map errMsg $ es
+ppError (NotAllowed e)   = putStrLn $ "NotAllowed: " ++ e
+ppError (GhcException e) = putStrLn $ "GhcException: " ++ e  -- TODO: can we actually recover from this?
+
+
+-- | Given the diagram's source code and options for the cairo
+--   backend, build the diagram (in the context of standard imports) and
+--   render it as requested
+buildDiagram :: String -> Options Cairo R2 -> IO ()
+buildDiagram source opts = do
+  tmpDir <- getTemporaryDirectory
+  (tmp, h) <- openTempFile tmpDir "Diagram.lhs"
+  hPutStr h (diagramFileHeader $ takeBaseName tmp)
+  hPutStr h source
+  hClose h
+  compileExample tmp opts
+  removeFile tmp
+
+diagramFileHeader :: String -> String
+diagramFileHeader modName = unlines $
+  [ "> {-# LANGUAGE NoMonomorphismRestriction, DeriveDataTypeable #-}"
+  , "> module " ++ modName ++ " where"
+  , "> import Diagrams.Prelude"
+  , "> import Diagrams.Backend.Cairo"
+  , "> import Data.Typeable"
+  ]
+-}
