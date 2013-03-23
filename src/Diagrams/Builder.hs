0ebcf85d5628fee99466ad7f4b4d8d2d84b76502
Brent Yorgey <byorgey@cis.upenn.edu>
2013-03-22 21:15:04 -0400

    comment formatting

--- src/Diagrams/Builder.hs
+++ src/Diagrams/Builder.hs
@@ -75,15 +75,18 @@ deriving instance Typeable Any
 
 -- | Set up the module to be interpreted, in the context of the
 --   necessary imports.
-setDiagramImports :: MonadInterpreter m
-                  => String      -- ^ Filename of the module containing the diagrams
-                  -> [String]    -- ^ Additional necessary
-                                 --   imports. @Prelude@,
-                                 --   @Diagrams.Prelude@,
-                                 --   @Diagrams.Core.Types@,
-                                 --   and @Data.Monoid@ are included
-                                 --   by default.
-                  -> m ()
+setDiagramImports
+  :: MonadInterpreter m
+  => String
+     -- ^ Filename of the module containing the diagrams
+
+  -> [String]
+     -- ^ Additional necessary imports. @Prelude@, @Diagrams.Prelude@,
+     --   @Diagrams.Core.Types@, and @Data.Monoid@ are included by
+     --   default.
+
+  -> m ()
+
 setDiagramImports m imps = do
     loadModules [m]
     setTopLevelModules [takeBaseName m]
@@ -96,17 +99,18 @@ setDiagramImports m imps = do
 
 -- | Interpret a diagram expression based on the contents of a given
 --   source file, using some backend to produce a result.
-interpretDiagram :: forall b v.
-                  ( Typeable b, Typeable v
-                  , InnerSpace v, OrderedField (Scalar v), Backend b v
-                  )
-               => b             -- ^ Backend token
-               -> v             -- ^ Dummy vector to identify the vector space
-               -> Options b v   -- ^ Rendering options
-               -> FilePath      -- ^ Filename of the module containing the example
-               -> [String]      -- ^ Additional imports needed
-               -> String        -- ^ Expression of type @Diagram b v@ to be compiled
-               -> IO (Either InterpreterError (Result b v))
+interpretDiagram
+  :: forall b v.
+     ( Typeable b, Typeable v
+     , InnerSpace v, OrderedField (Scalar v), Backend b v
+     )
+  => b             -- ^ Backend token
+  -> v             -- ^ Dummy vector to identify the vector space
+  -> Options b v   -- ^ Rendering options
+  -> FilePath      -- ^ Filename of the module containing the example
+  -> [String]      -- ^ Additional imports needed
+  -> String        -- ^ Expression of type @Diagram b v@ to be compiled
+  -> IO (Either InterpreterError (Result b v))
 interpretDiagram b _ opts m imps dexp =
     runInterpreter $ do
       setDiagramImports m imps
@@ -139,64 +143,60 @@ data BuildResult b v x =
 --   module and interpreting the given expression.  Can return either
 --   a parse error if the source does not parse, an interpreter error,
 --   or the final result.
-buildDiagram :: ( Typeable b, Typeable v
-                , InnerSpace v, OrderedField (Scalar v), Backend b v
-                , Show (Options b v)
-                )
-             => b              -- ^ Backend token
-             -> v              -- ^ Dummy vector to fix the vector type
-             -> Options b v    -- ^ Backend-specific options to use
-             -> [String]       -- ^ Source code snippets.  Each should
-                               --   be a syntactically valid Haskell
-                               --   module.  They will be combined
-                               --   intelligently, /i.e./ not just
-                               --   pasted together textually but
-                               --   combining pragmas, imports,
-                               --   /etc./ separately.
-             -> String         -- ^ Diagram expression to interpret
-             -> [String]       -- ^ Extra @LANGUAGE@ pragmas to use
-                               --   (@NoMonomorphismRestriction@ is used
-                               --   by default.)
-             -> [String]       -- ^ Additional imports
-                               --   ("Diagrams.Prelude" is imported by
-                               --   default).
-             -> (String -> IO (x, Maybe (Options b v -> Options b v)))
-                               -- ^ A function to decide whether a
-                               --   particular diagram needs to be
-                               --   regenerated.  It will be passed
-                               --   the final assembled source for the
-                               --   diagram (but with the module name
-                               --   set to @Main@ instead of something
-                               --   auto-generated, so that hashing
-                               --   the source will produce consistent
-                               --   results across runs). It can
-                               --   return some information (such as a
-                               --   hash of the source) via the @x@
-                               --   result, which will be passed
-                               --   through to the result of
-                               --   'buildDiagram'.  More importantly,
-                               --   it decides whether the diagram
-                               --   should be built: a result of
-                               --   'Just' means the diagram /should/
-                               --   be built; 'Nothing' means it
-                               --   should not. In the case that it
-                               --   should be built, it returns a
-                               --   function for updating the
-                               --   rendering options.  This could be
-                               --   used, /e.g./, to request a
-                               --   filename based on a hash of the
-                               --   source.
-                               --
-                               --   Two standard decision functions
-                               --   are provided for convenience:
-                               --   'alwaysRegenerate' returns no
-                               --   extra information and always
-                               --   decides to regenerate the diagram;
-                               --   'hashedRegenerate' creates a hash
-                               --   of the diagram source and looks
-                               --   for a file with that name in a
-                               --   given directory.
-             -> IO (BuildResult b v x)
+buildDiagram
+  :: ( Typeable b, Typeable v
+     , InnerSpace v, OrderedField (Scalar v), Backend b v
+     , Show (Options b v)
+     )
+  => b
+     -- ^ Backend token
+
+  -> v
+     -- ^ Dummy vector to fix the vector type
+
+  -> Options b v
+     -- ^ Backend-specific options to use
+
+  -> [String]
+     -- ^ Source code snippets.  Each should be a syntactically valid
+     --   Haskell module.  They will be combined intelligently, /i.e./
+     --   not just pasted together textually but combining pragmas,
+     --   imports, /etc./ separately.
+
+  -> String
+     -- ^ Diagram expression to interpret
+
+  -> [String]
+     -- ^ Extra @LANGUAGE@ pragmas to use (@NoMonomorphismRestriction@
+     --   is used by default.)
+
+  -> [String]
+     -- ^ Additional imports ("Diagrams.Prelude" is imported by
+     --   default).
+
+  -> (String -> IO (x, Maybe (Options b v -> Options b v)))
+     -- ^ A function to decide whether a particular diagram needs to
+     --   be regenerated.  It will be passed the final assembled
+     --   source for the diagram (but with the module name set to
+     --   @Main@ instead of something auto-generated, so that hashing
+     --   the source will produce consistent results across runs). It
+     --   can return some information (such as a hash of the source)
+     --   via the @x@ result, which will be passed through to the
+     --   result of 'buildDiagram'.  More importantly, it decides
+     --   whether the diagram should be built: a result of 'Just'
+     --   means the diagram /should/ be built; 'Nothing' means it
+     --   should not. In the case that it should be built, it returns
+     --   a function for updating the rendering options.  This could
+     --   be used, /e.g./, to request a filename based on a hash of
+     --   the source.
+     --
+     --   Two standard decision functions are provided for
+     --   convenience: 'alwaysRegenerate' returns no extra information
+     --   and always decides to regenerate the diagram;
+     --   'hashedRegenerate' creates a hash of the diagram source and
+     --   looks for a file with that name in a given directory.
+
+  -> IO (BuildResult b v x)
 buildDiagram b v opts source dexp langs imps shouldRegen = do
   let source'   = map unLit source
   case createModule
@@ -259,25 +259,23 @@ alwaysRegenerate _ = return ((), Just id)
 --   hash.  (Most likely, one would want to set the requested output
 --   file to the hash followed by some extension.)  It also returns
 --   the generated hash.
-hashedRegenerate :: (String -> a -> a)  -- ^ A function for computing
-                                        --   an update to rendering
-                                        --   options, given a new base
-                                        --   filename computed from a
-                                        --   hash of the diagram
-                                        --   source.
-                 -> FilePath            -- ^ The directory in which to
-                                        --   look for generated files
-                 -> String              -- ^ The \"source\" to
-                                        -- hash. Note that this does
-                                        -- not actually have to be
-                                        -- valid source code.  A
-                                        -- common trick is to
-                                        -- concatenate the actual
-                                        -- source code with String
-                                        -- representations of any
-                                        -- other information on which
-                                        -- the diagram depends.
-                 -> IO (String, Maybe (a -> a))
+hashedRegenerate
+  :: (String -> a -> a)
+     -- ^ A function for computing an update to rendering options,
+     --   given a new base filename computed from a hash of the
+     --   diagram source.
+
+  -> FilePath
+     -- ^ The directory in which to look for generated files
+
+  -> String
+     -- ^ The \"source\" to hash. Note that this does not actually
+     -- have to be valid source code.  A common trick is to
+     -- concatenate the actual source code with String representations
+     -- of any other information on which the diagram depends.
+
+  -> IO (String, Maybe (a -> a))
+
 hashedRegenerate upd dir src = do
   let fileBase = hashStr src
   files <- getDirectoryContents dir
