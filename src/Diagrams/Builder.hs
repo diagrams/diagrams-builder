9d6cc18b17996cd89db71a61cea7964dd7121d34
Brent Yorgey <byorgey@cis.upenn.edu>
2013-11-15 15:19:21 -0500

    fix braino --- want to HIDE (<.>) not IMPORT it =(

--- src/Diagrams/Builder.hs
+++ src/Diagrams/Builder.hs
@@ -62,7 +62,7 @@ import           Language.Haskell.Interpreter        hiding (ModuleName)
 
 import           Diagrams.Builder.CmdLine
 import           Diagrams.Builder.Modules
-import           Diagrams.Prelude                    ((<.>))
+import           Diagrams.Prelude                    hiding ((<.>))
 import           Language.Haskell.Interpreter.Unsafe (unsafeRunInterpreterWithArgs)
 import           System.Environment                  (getEnvironment)
 
