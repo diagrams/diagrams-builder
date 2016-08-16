61687c36bf207980205d4365ec4fcb1b46f33bfc
Brent Yorgey <byorgey@gmail.com>
2016-08-16 12:06:08 -0500

    fix import name

--- src/tools/diagrams-builder-svg.hs
+++ src/tools/diagrams-builder-svg.hs
@@ -7,7 +7,7 @@ module Main where
 import           Diagrams.Backend.SVG
 import           Diagrams.Builder
 import           Diagrams.Prelude       hiding (height, width)
-import           Graphics.SVG
+import           Graphics.Svg
 
 import           System.Console.CmdArgs
 import           System.Directory       (copyFile, createDirectoryIfMissing)
